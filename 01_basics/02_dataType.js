d7cd2a4ea66d6cbea2a1e1bc0a4a2b58abe66011
Md Ainain Ahmed <108012966+mdainainahmed@users.noreply.github.com>
2023-09-11 20:10:14 +0000

    dataTypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,32 @@
+// number => 2 to pow 53
+const accountId = 12453
+
+// bigInt
+
+// string => "" / ''
+let accountEmail = "ainain@google.com"
+var accountPassword = "123@ainain"
+accountCity = 'gaya'
+
+// boolean  => true / false
+let isLoggedIn = true
+
+// undefined => variable is not initialized, or value is not defined
+let accountState
+
+// null => standalone value
+let accountCountry = null;  // type object
+
+console.log(typeof(accountId))
+console.table([typeof(accountId), typeof(accountEmail), typeof(accountPassword), typeof(isLoggedIn), typeof(accountCity), typeof(accountState), typeof(accountCountry)])
+
+// symbol => unique
+
+// object
+
+/*          <--- Important for interview --->
+    console.log(typeof undefined)    // undefined
+    console.log(typeof null)         // object 
+*/
+console.log(typeof accountState)    
+console.log(typeof accountCountry)   
