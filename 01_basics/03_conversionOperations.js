686159329581918ea3cec7c98c09daf600ba0129
Md Ainain Ahmed <108012966+mdainainahmed@users.noreply.github.com>
2023-09-12 15:27:49 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+console.log(1+2)
+
+let val1 = "1" + 2 + 3
+console.log(val1, typeof val1)
+
+let val2 = 1 + 3 + "2"
+console.log(val2, typeof val2)
+
+let val3 = "10" - 3
+console.log(val3, typeof val3)
+
+let val4 = 3 - "2"
+console.log(val4, typeof val4)
+
+let val5 = 0 + true 
+console.log(val5, typeof val5) 
+
+let val6 = "6" - "3"
+console.log(val6, typeof val6)
