ef3d8b900c18dae9001de0c078ece28ec1ce3388
Md Ainain Ahmed <108012966+mdainainahmed@users.noreply.github.com>
2023-09-12 15:39:13 +0000

    comparisions

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,7 +1,11 @@
-console.log(null >= 0)
-console.log(null == 0)
-console.log(null < 0)
+// console.log(null >= 0)
+// console.log(null == 0)
+// console.log(null < 0)
 
-console.log(undefined > 0)
-console.log(undefined == 0)
-console.log(undefined < 0)
+// console.log(undefined > 0)
+// console.log(undefined == 0)
+// console.log(undefined < 0)
+
+// === 
+console.log("02" == 2)
+console.log("02" === 2)
