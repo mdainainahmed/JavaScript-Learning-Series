b88342132161063f961cb23e2a32376bf03f9f45
Md Ainain Ahmed <108012966+mdainainahmed@users.noreply.github.com>
2023-09-14 19:42:35 +0000

    numbers

---
+++ 01_basics/06_numbers.js
@@ -0,0 +1,12 @@
+// const num = 400
+console.log(num)
+// console.log(num.toString())
+
+const num1 = new Number(67.56732)
+// console.log(num1.toFixed(2))
+
+const num2 = 10000329
+// console.log(num2.toLocaleString('en-IN'))
+
+
+// ++++++++++++++++++++ Maths ++++++++++++++++++++++
