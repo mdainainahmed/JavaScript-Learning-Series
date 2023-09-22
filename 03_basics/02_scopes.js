a2bb474e516dcf1cf4a1c4c442eee6a165957712
Md Ainain Ahmed <108012966+mdainainahmed@users.noreply.github.com>
2023-09-22 22:16:23 +0000

    scopes.js

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,44 @@
+
+// let a = 40
+// const b = 150
+
+// {
+//     let a = 50
+//     const b = 230
+//     a = 60
+//     console.log(a,b);
+// }
+
+// console.log(a, b);
+
+// function one(){
+//     let username = "Ainain"
+
+//     function two(){
+//         let courseName = "jsSeries"
+
+//         console.log(username);
+//     }
+//     two()
+
+//     console.log(courseName)
+// }
+
+// one()
+// console.log(username, courseName)
+
+
+
+
+// ++++++++++++++++++++++++++ Interesting +++++++++++++++++++++
+
+// addOne(5)
+// function addOne( num ){
+//     return num + 1
+// }
+
+
+// addTwo(5)
+// const addTwo = function( num ){
+//     return num + 2
+// }
