d16beab97d30f212cf45ceeb62a6255f4244d722
Md Ainain Ahmed <108012966+mdainainahmed@users.noreply.github.com>
2023-09-26 18:01:28 +0000

    forIn.js

---
+++ 04_iterators/02_objectsIteration.js
@@ -0,0 +1,35 @@
+// const myObj = {
+//     name: "Ainain",
+//     age: 22,
+//     fullName: {
+//         firstName: "Md",
+//         lastName: "Ahmed"
+//     },
+//     getAddress: function(){
+//         console.log("Gaya, Bihar")
+//     } 
+// } 
+
+// for (const key in myObj) {
+//     console.log( `${key} -> ${myObj[key]}` )
+// }
+
+
+// const myMap = new Map()
+// myMap.set("javascript", "js")
+// myMap.set("c++", "cpp")
+// myMap.set("ruby", "rb")
+
+// for (const key in myMap) {
+//     console.log(myMap[key])
+// }
+
+// const myArr = [1,2,3,4,5]
+// for (const key in myArr) {
+//     console.log(myArr[key]);
+// }
+
+// const myString = "My name is Md Ainain Ahmed"
+// for (const key in myString) {
+//     console.log(myString[key]);
+// }
