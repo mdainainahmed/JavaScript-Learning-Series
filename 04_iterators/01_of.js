a4f3fc4917a7d1c78ddaf16b02d978dd5575e989
Md Ainain Ahmed <108012966+mdainainahmed@users.noreply.github.com>
2023-09-26 18:01:18 +0000

    forOf.js

---
+++ 04_iterators/01_of.js
@@ -0,0 +1,39 @@
+// const myArr = ["Ainain", 12, { name: "Ahmed", age: 21 }]
+
+// for( const values of myArr ){
+//     console.log(values)
+// }
+
+// ++++++++++++++++++++++ Map ++++++++++++++++++++++++
+// const myMap = new Map();
+// myMap.set("IN", "India")
+// myMap.set("US", "United States")
+// myMap.set("anObj", { name: "Ainain", age: 22 })
+
+// // for( const key of myMap ){
+// //     console.log(key);
+// // }
+
+// for( const [key, value] of myMap ){
+//     console.log(key, " -> ", value)
+// }
+
+
+
+
+// ++++++++++++++++++++++++++++ of doesn't work with objects ++++++++++++++++++
+// const myObj = {
+//     name: "Ainain",
+//     age: 22,
+//     fullName: {
+//         firstName: "Md",
+//         lastName: "Ahmed"
+//     },
+//     getAddress: function(){
+//         console.log("Gaya, Bihar")
+//     } 
+// }
+
+// for( const [key,value] of myObj ){
+//     console.log(key, "->", value)
+// }
