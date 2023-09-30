ec01f7b36bb8266f85adaaf27a15dbd9c09ca9d7
Md Ainain Ahmed <108012966+mdainainahmed@users.noreply.github.com>
2023-09-30 19:43:46 +0000

    updated script.js

--- 07_projects/04_GuessTheNumber/script.js
+++ 07_projects/04_GuessTheNumber/script.js
@@ -1,60 +1,75 @@
-let prevGuess = []
-const number = parseInt(Math.random() * 100 + 1)
+let guessessArr = [];
+let randomNumber;
+const form = document.querySelector('form');
+const inputVal = document.querySelector('#guessField');
+const prevGuesses = document.querySelector('.guesses');
+const guessessRemaining = document.querySelector('.lastResult');
+const result = document.querySelector('.lowOrHi');
 
-const form = document.querySelector('form')
-form.addEventListener('submit', (event) => {
-  event.preventDefault()
+let play = true;
 
-  const result = document.querySelector('.lowOrHi')
-  const guessRemaining = document.querySelector('.lastResult')
-  const previousGesses = document.querySelector('.guesses')
+if (play) {
+  randomNumber = generateRandom();
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    // check valid
+    const guess = parseInt(inputVal.value);
+    validationCheck(guess);
+  });
+}
 
-  // get input value
-  const guessVal = document.querySelector('#guessField').value
-
-  // check input value is valid or not
-  if (guessVal === '' || guessVal < 1 || guessVal > 100 || isNaN(guessVal)) {
-    result.innerHTML = `<span>Please Enter a valid Guess ${guessVal}</span>`
-    result.style.backgroundColor = 'red'
-    return
-  } else
-    result.innerHTML = ''
-
-
-  // check if input value matched
-  if (guessVal == number) {
-    result.innerHTML = `<span>Hurray! You matched the number ${guessVal}</span>`
-    result.style.backgroundColor = 'yellow'
-    result.style.color = '#000'
-    prevGuess = []
-    previousGesses.innerHTML = '<span></span>'
-    guessRemaining.innerHTML = '<span>10</span>'
-    return
+function validationCheck(guess) {
+  if (guess < 1) displayMessage('Input < 1');
+  else if (guess > 100) displayMessage('Input > 100');
+  else if (guess === '') displayMessage('Enter Valid Number');
+  else if (isNaN(guess)) displayMessage('Input is NAN');
+  else if (guessessArr.includes(guess)) displayMessage('Enter unique values');
+  else if (guess === randomNumber) {
+    displayMessage('Hurray! You have Matched');
+    endGame();
+  } else {
+    displayMessage(' ');
+    updateArr(guess);
   }
-  // check input value is already present
-  if (prevGuess.includes(guessVal)) {
-    result.innerHTML = `<span>Enter a Unique Value ${guessVal}</span>`
-    result.style.backgroundColor = 'red'
-    return
+}
+
+function displayMessage(message) {
+  result.innerHTML = message;
+}
+
+function updateArr(guess) {
+  guessessArr.push(guess);
+  // update Previous Guesses Section
+  updatePreviousGuesses();
+}
+
+function updatePreviousGuesses() {
+  prevGuesses.innerHTML = `${guessessArr.map((val) => val)}`;
+  // update remaining guesses
+  updateRemainingGuesses();
+}
+
+function updateRemainingGuesses() {
+  const leftGuesses = 10 - guessessArr.length;
+  guessessRemaining.innerHTML = `${leftGuesses}`;
+  // check if guesses left
+  guessesLeft(leftGuesses);
+}
+
+function guessesLeft(count) {
+  if (count == 0) {
+    displayMessage('No Guesses Left');
+    endGame();
   }
+}
 
-  // push in prevGuess
-  prevGuess.push(guessVal)
+function endGame() {
+  guessessArr = [];
+  prevGuesses.innerHTML = '';
+  guessessRemaining.innerHTML = 10;
+  randomNumber = generateRandom();
+}
 
-  // update Previous Gesses
-  previousGesses.innerHTML = `${prevGuess.map((val) => val)}`
-
-  // size of prevGuess
-  const currSize = prevGuess.length
-
-  // update Guesses Remaining
-  const val = 10 - prevGuess.length
-  guessRemaining.innerHTML = `<span>${val}</span>`
-
-  // check if no guess left : preCheck
-  if (prevGuess.length == 10) {
-    result.innerHTML = `<span>All guesses completed. ${val} guesses left....</span>`
-    result.style.backgroundColor = 'red'
-    prevGuess = []
-  }
-});
+function generateRandom() {
+  return parseInt(Math.random() * 100 + 1);
+}
